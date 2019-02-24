fix the formula mistakes in mu

IAGMM.py
@@ -2,6 +2,7 @@ import time
 import copy
 import mpmath
 import numpy as np
+from numpy.linalg import inv
 from utils import *
 from scipy import stats
 
@@ -65,9 +66,8 @@ def infinte_mixutre_model(X, Nsamples=100, Nint=50, anneal=False):
     mu = np.zeros((1, D))      # initialise the means
     mu_irr = np.zeros((1, D))
     s_l = np.zeros((1, D))    # initialise the precisions
-    s_l_irr = np.zeros((1, D))
     s_r = np.zeros((1, D))
-    s_r_irr = np.zeros((1, D))
+    s_irr = np.zeros((1, D))
     n = np.array([N])  # initialise the occupation numbers
 
     # set first mu to the mean of all data
@@ -83,9 +83,8 @@ def infinte_mixutre_model(X, Nsamples=100, Nint=50, anneal=False):
     w_l = np.array([np.squeeze(draw_gamma(0.5, 2*vary[k])) for k in range(D)])
     w_r = np.array([np.squeeze(draw_gamma(0.5, 2*vary[k])) for k in range(D)])
     s_l[0, :] = np.array([np.squeeze(draw_gamma(beta_l[k]/2, 2/(beta_l[k]*w_l[k]))) for k in range(D)])
-    s_l_irr[0, :] = np.array([np.squeeze(draw_gamma(beta_l[k]/2, 2/(beta_l[k]*w_l[k]))) for k in range(D)])
     s_r[0, :] = np.array([np.squeeze(draw_gamma(beta_r[k]/2, 2/(beta_r[k]*w_r[k]))) for k in range(D)])
-    s_r_irr[0, :] = np.array([np.squeeze(draw_gamma(beta_r[k]/2, 2/(beta_r[k]*w_r[k]))) for k in range(D)])
+    s_irr[0, :] = np.array([np.squeeze(draw_gamma(beta_r[k]/2, 2/(beta_r[k]*w_r[k]))) for k in range(D)])
     delta_a = np.array([np.squeeze(draw_gamma(2, 0.5)) for k in range(D)])
     delta_b = np.array([np.squeeze(draw_gamma(2, 0.5)) for k in range(D)])
     lam = draw_MVNormal(mean=muy, cov=vary)
@@ -109,20 +108,45 @@ def infinte_mixutre_model(X, Nsamples=100, Nint=50, anneal=False):
         for k in range(D):
             vary[k] = np.var(X[:, k])
         precisiony = 1/vary
-        posterior_z = draw_posterior_z(X, pi, rho, mu, s_l, s_r, mu_irr, s_l_irr, s_r_irr, N, M, D)
+        posterior_z = draw_posterior_z(X, pi, rho, mu, s_l, s_r, mu_irr, s_irr, N, M, D)
+
+        # draw z from Bernoulli distribution
+        for i in range(N):
+            for j in range(M):
+                for k in range(D):
+                    z_indicators[i, j, k] = draw_Bernoulli(posterior_z[i, j, k])
 
         # the observations belonged to class j
         Xj = [X[np.where(c==j), :] for j, nj in enumerate(n)]
+        mu_cache = mu
+        mu = np.zeros((M, D))
         j = 0
         # draw mu and mu_irr from posterior
         for x, nj in zip(Xj, n):
             x = x[0]
             # for every dimensionality, compute the posterior distribution of mu_jk
             for k in range(D):
-                mu[j, k] = MH_Sampling_posterior_mu_jk(mu[j, k], s_l[j, k], s_r[j, k], mu_irr[j, k], s_l_irr[j, k],s_r_irr[j, k],
-                                                       r, lam, posterior_z, j, k, x)
-                mu_irr[j, k] = MH_Sampling_posterior_mu_jk(mu_irr[j, k], s_l_irr[j, k],s_r_irr[j, k], mu[j, k], s_l[j, k], s_r[j, k],
-                                                           r, lam, 1-posterior_z, j, k, x)
+                x_k = x[:, k]
+                rel_x_k = x_k[np.reshape((z_indicators[:, :, k] == 1), (100))]
+                irr_x_k = x_k[np.reshape((z_indicators[:, :, k] == 0), (100))]
+                rel_x_k_num = rel_x_k.shape[0]
+                irr_x_k_num = irr_x_k.shape[0]
+                # p represent the number of x_ik < mu_jk where z_ik = 1
+                p = rel_x_k[rel_x_k < mu_cache[j][k]].shape[0]
+                # q represent the number of x_ik >= mu_jk, q = n - p, while z_ik = 1
+                q = rel_x_k[rel_x_k >= mu_cache[j][k]].shape[0]
+                # x_l_sum represents the sum from i to n of x_ik, where x_ik < mu_jk and z_ik = 1
+                rel_x_l_sum = np.sum(rel_x_k[rel_x_k < mu_cache[j][k]])
+                # x_r_sum represents the sum from i to n of x_ik, where x_ik >= mu_jk and z_ik = 1
+                rel_x_r_sum = np.sum(rel_x_k[rel_x_k >= mu_cache[j][k]])
+                r_n = r[k] + p * s_l[j, k] + q * s_r[j, k]
+                mu_n = (s_l[j, k] * rel_x_l_sum + s_r[j, k] * rel_x_r_sum + r[k] * lam[k])/r_n
+                mu[j, k] = draw_normal(mu_n, 1/r_n)
+
+                irr_x_sum = np.sum(irr_x_k[irr_x_k >= mu_cache[j][k]])
+                r_n_irr = r[k] + irr_x_k_num * s_irr[j, k]
+                mu_n_irr = (s_irr[j, k] * irr_x_sum + r[k] * lam[k])/r_n_irr
+                mu_irr[j, k] = draw_normal(mu_n_irr, 1/r_n_irr)
             j += 1
 
         # draw lambda from posterior
@@ -148,7 +172,7 @@ def infinte_mixutre_model(X, Nsamples=100, Nint=50, anneal=False):
         # draw alpha from posterior. Because its not standard form, using ARS to sampling
         alpha = draw_alpha(M, N)
 
-        # draw sj from posterior (
+        # draw sj from posterior
         for j, nj in enumerate(n):
             Xj = X[np.where(c == j), :][0]
             for k in range(D):
@@ -161,77 +185,66 @@ def infinte_mixutre_model(X, Nsamples=100, Nint=50, anneal=False):
                 s_r_irr[j, k] = MH_Sampling_posterior_sljk(mu_irr[j, k], s_l_irr[j, k], s_r_irr[j, k], mu[j, k], s_l[j, k], s_r[j, k],
                                 beta_r, w_r, 1-posterior_z, j, k, Xj)
 
+        # # draw w from posterior
+        # w_l = np.array([np.squeeze(draw_gamma(0.5 *(2*M*beta_l[k]+1),\
+        #                 2/(vary[k] + beta_l[k]*np.sum(s_l, axis=0)[k] + beta_l[k]*np.sum(s_l_irr, axis=0)[k]
+        #                 ))) for k in range(D)])
+        # w_r = np.array([np.squeeze(draw_gamma(0.5 *(2*M*beta_r[k]+1),\
+        #                 2/(vary[k] + beta_r[k]*np.sum(s_r, axis=0)[k] + beta_r[k]*np.sum(s_r_irr, axis=0)[k]
+        #                 ))) for k in range(D)])
+        #
+        # # draw beta from posterior. Because its not standard form, using ARS to sampling.
+        # beta_l = np.array([draw_beta_ars(w_l, s_l, s_l_irr, M, k)[0] for k in range(D)])
+        # beta_r = np.array([draw_beta_ars(w_l, s_r, s_r_irr, M, k)[0] for k in range(D)])
+
         # compute the unrepresented probability
-        # p_unrep = (alpha / (N - 1.0 + alpha)) * integral_approx(X, lam, r, beta_l, beta_r, w_l, w_r,
-        #                                                         delta_a, delta_b)
-        p_unrep = (alpha / (N - 1.0 + alpha)) * integral_approx(X, lam, r, beta_l, beta_r, w_l, w_r )
-        p_indicators_prior = np.outer(np.ones(M + 1), p_unrep)
+        # p_unrep = (alpha / (N - 1.0 + alpha)) * integral_approx(X, lam, r, beta_l, beta_r, w_l, w_r, delta_a, delta_b)
+        # p_unrep = (alpha / (N - 1.0 + alpha)) * integral_approx(X, lam, r, beta_l, beta_r, w_l, w_r )
+        # p_indicators_prior = np.outer(np.ones(M + 1), p_unrep)
 
         # for the represented components
-        for j in range(M):
-            # n-i,j : the number of oberservations, excluding Xi, that are associated with component j
-            nij = n[j] - (c == j).astype(int)
-            idx = np.argwhere(nij > 0)
-            idx = idx.reshape(idx.shape[0])
-            likelihood_for_associated_data = np.ones(len(idx))
-            # for i in range(len(idx)):
-            #     for k in range(D):
-            #         Y = 0
-            #         if X[i, k] < mu[j, k]:
-            #             Y += rho[j, k] / (np.power(s_l[j, k], -0.5) + np.power(s_r[j, k], -0.5))* \
-            #                         np.exp(- 0.5 * s_l[j, k] * np.power(X[i, k] - mu[j, k], 2))
-            #         else:
-            #             Y += rho[j, k] / (np.power(s_l[j, k], -0.5) + np.power(s_r[j][k], -0.5))* \
-            #                         np.exp(- 0.5 * s_r[j, k] * np.power(X[i, k] - mu[j, k], 2))
-            #         if X[i, k] < mu_irr[j, k]:
-            #             Y += (1 - rho[j, k]) / (np.power(s_l_irr[j, k], -0.5) + np.power(s_r_irr[j, k], -0.5)) \
-            #                       * np.exp(- 0.5 * s_l_irr[j, k] * (X[i, k] - mu_irr[j, k]) ** 2)
-            #         else:
-            #             Y += (1 - rho[j, k]) / (np.power(s_l_irr[j, k], -0.5) + np.power(s_r_irr[j, k], -0.5)) \
-            #                       * np.exp(- 0.5 * s_r_irr[j, k] * (X[i, k] - mu_irr[j, k]) ** 2)
-            #         likelihood_for_associated_data[i] *= Y
-            # p_indicators_prior[j, idx] = nij[idx] / (N - 1.0 + alpha) * likelihood_for_associated_data
-            likelihood_for_associated_data = np.ones(len(idx))
-            for i in range(len(idx)):
-                for k in range(D):
-                    if X[i][k] < mu[j][k]:
-                        likelihood_for_associated_data[i] *= 1 / (np.power(s_l[j][k], -0.5) + np.power(s_r[j][k], -0.5))* \
-                                    np.exp(- 0.5 * s_l[j][k] * np.power(X[i][k] - mu[j][k], 2))
-                    else:
-                        likelihood_for_associated_data[i] *= 1 / (np.power(s_l[j][k], -0.5) + np.power(s_r[j][k], -0.5))* \
-                                    np.exp(- 0.5 * s_r[j][k] * np.power(X[i][k] - mu[j][k], 2))
-            p_indicators_prior[j, idx] = nij[idx]/(N - 1.0 + alpha)*likelihood_for_associated_data
+        # for j in range(M):
+        #     # n-i,j : the number of oberservations, excluding Xi, that are associated with component j
+        #     nij = n[j] - (c == j).astype(int)
+        #     idx = np.argwhere(nij > 0)
+        #     idx = idx.reshape(idx.shape[0])
+        #     likelihood_for_associated_data = np.ones(len(idx))
+        #     # for i in range(len(idx)):
+        #     #     for k in range(D):
+        #     #         Y = 0
+        #     #         if X[i, k] < mu[j, k]:
+        #     #             Y += rho[j, k] / (np.power(s_l[j, k], -0.5) + np.power(s_r[j, k], -0.5))* \
+        #     #                         np.exp(- 0.5 * s_l[j, k] * np.power(X[i, k] - mu[j, k], 2))
+        #     #         else:
+        #     #             Y += rho[j, k] / (np.power(s_l[j, k], -0.5) + np.power(s_r[j][k], -0.5))* \
+        #     #                         np.exp(- 0.5 * s_r[j, k] * np.power(X[i, k] - mu[j, k], 2))
+        #     #         if X[i, k] < mu_irr[j, k]:
+        #     #             Y += (1 - rho[j, k]) / (np.power(s_l_irr[j, k], -0.5) + np.power(s_r_irr[j, k], -0.5)) \
+        #     #                       * np.exp(- 0.5 * s_l_irr[j, k] * (X[i, k] - mu_irr[j, k]) ** 2)
+        #     #         else:
+        #     #             Y += (1 - rho[j, k]) / (np.power(s_l_irr[j, k], -0.5) + np.power(s_r_irr[j, k], -0.5)) \
+        #     #                       * np.exp(- 0.5 * s_r_irr[j, k] * (X[i, k] - mu_irr[j, k]) ** 2)
+        #     #         likelihood_for_associated_data[i] *= Y
+        #     # p_indicators_prior[j, idx] = nij[idx] / (N - 1.0 + alpha) * likelihood_for_associated_data
+        #     likelihood_for_associated_data = np.ones(len(idx))
+        #     for i in range(len(idx)):
+        #         for k in range(D):
+        #             if X[i][k] < mu[j][k]:
+        #                 likelihood_for_associated_data[i] *= 1 / (np.power(s_l[j][k], -0.5) + np.power(s_r[j][k], -0.5))* \
+        #                             np.exp(- 0.5 * s_l[j][k] * np.power(X[i][k] - mu[j][k], 2))
+        #             else:
+        #                 likelihood_for_associated_data[i] *= 1 / (np.power(s_l[j][k], -0.5) + np.power(s_r[j][k], -0.5))* \
+        #                             np.exp(- 0.5 * s_r[j][k] * np.power(X[i][k] - mu[j][k], 2))
+        #     p_indicators_prior[j, idx] = nij[idx]/(N - 1.0 + alpha)*likelihood_for_associated_data
         # for j in range(p_indicators_prior.shape[0]):
         #     p_indicators_prior[j, np.where(p_indicators_prior[j] < 0.00001)] = 0.000001
         #
-        # print("__")
-        # print(p_indicators_prior)
-        # for j in range(p_indicators_prior.shape[0]):
-        #     if j == 0:
-        #         pass
-        #     else:
-        #         print(np.where(p_indicators_prior[j]>0.00001))
-        # print("__")
         # stochastic indicator (we could have a new component)
-        c = np.hstack(draw_indicator(p_indicators_prior))
+        # c = np.hstack(draw_indicator(p_indicators_prior))
+        #
 
-        # draw w from posterior
-        w_l = np.array([np.squeeze(draw_gamma(0.5 *(2*M*beta_l[k]+1),\
-                        2/(vary[k] + beta_l[k]*np.sum(s_l, axis=0)[k] + beta_l[k]*np.sum(s_l_irr, axis=0)[k]
-                        ))) for k in range(D)])
-        w_r = np.array([np.squeeze(draw_gamma(0.5 *(2*M*beta_r[k]+1),\
-                        2/(vary[k] + beta_r[k]*np.sum(s_r, axis=0)[k] + beta_r[k]*np.sum(s_r_irr, axis=0)[k]
-                        ))) for k in range(D)])
 
-        # draw beta from posterior. Because its not standard form, using ARS to sampling.
-        beta_l = np.array([draw_beta_ars(w_l, s_l, s_l_irr, M, k)[0] for k in range(D)])
-        beta_r = np.array([draw_beta_ars(w_l, s_r, s_r_irr, M, k)[0] for k in range(D)])
 
-        # draw z from Bernoulli distribution
-        for i in range(N):
-            for j in range(M):
-                for k in range(D):
-                    z_indicators[i, j, k] = draw_Bernoulli(posterior_z[i, j, k])
         f = np.array([[np.count_nonzero(z_indicators[:, j, k] == 1) for k in range(D)] for j in range(M)])
 
         # draw rho from posterior
@@ -253,8 +266,8 @@ def infinte_mixutre_model(X, Nsamples=100, Nint=50, anneal=False):
             news_l = np.array([np.squeeze(draw_gamma(beta_l[k] / 2, 2 / (beta_l[k] * w_l[k]))) for k in range(D)])
             news_r = np.array([np.squeeze(draw_gamma(beta_r[k] / 2, 2 / (beta_r[k] * w_r[k]))) for k in range(D)])
             newmu_irr = np.array([np.squeeze(draw_normal(lam[k], 1 / r[k])) for k in range(D)])
-            news_l_irr = np.array([np.squeeze(draw_gamma(beta_l[k] / 2, 2 / (beta_l[k] * w_l[k]))) for k in range(D)])
-            news_r_irr = np.array([np.squeeze(draw_gamma(beta_r[k] / 2, 2 / (beta_r[k] * w_r[k]))) for k in range(D)])
+            # news_l_irr = np.array([np.squeeze(draw_gamma(beta_l[k] / 2, 2 / (beta_l[k] * w_l[k]))) for k in range(D)])
+            # news_r_irr = np.array([np.squeeze(draw_gamma(beta_r[k] / 2, 2 / (beta_r[k] * w_r[k]))) for k in range(D)])
             # newrho = draw_Beta_dist(delta_a, delta_b)
             newrho = np.array([0.975, 0.975])
             new_z_indicators = np.ones((N, 1, D))
@@ -263,8 +276,8 @@ def infinte_mixutre_model(X, Nsamples=100, Nint=50, anneal=False):
             s_l = np.concatenate((s_l, np.reshape(news_l, (1, D))))
             s_r = np.concatenate((s_r, np.reshape(news_r, (1, D))))
             mu_irr = np.concatenate((mu_irr, np.reshape(newmu_irr, (1, D))))
-            s_l_irr = np.concatenate((s_l_irr, np.reshape(news_l_irr, (1, D))))
-            s_r_irr = np.concatenate((s_r_irr, np.reshape(news_r_irr, (1, D))))
+            # s_l_irr = np.concatenate((s_l_irr, np.reshape(news_l_irr, (1, D))))
+            # s_r_irr = np.concatenate((s_r_irr, np.reshape(news_r_irr, (1, D))))
             rho = np.concatenate((rho, np.reshape(newrho, (1, D))))
             z_indicators = np.concatenate((z_indicators, new_z_indicators), axis=1)
             M = M + 1
@@ -282,8 +295,8 @@ def infinte_mixutre_model(X, Nsamples=100, Nint=50, anneal=False):
             s_r = np.delete(s_r, badidx, axis=0)
             rho = np.delete(rho, badidx, axis=0)
             mu_irr = np.delete(mu_irr, badidx, axis=0)
-            s_l_irr = np.delete(s_l_irr, badidx, axis=0)
-            s_r_irr = np.delete(s_r_irr, badidx, axis=0)
+            # s_l_irr = np.delete(s_l_irr, badidx, axis=0)
+            # s_r_irr = np.delete(s_r_irr, badidx, axis=0)
             z_indicators = np.delete(z_indicators, badidx, axis=1)
 
             # if the unrepresented compont removed is in the middle, make the sequential component indicators change

utils.py
@@ -6,10 +6,12 @@ from scipy.stats import multivariate_normal as mv_norm
 from scipy import special
 from ars import ARS
 import mpmath
+import cv2 as cv
 from numba import jit, njit, autojit, vectorize, guvectorize, float64, float32
 
 # the maximum positive integer for use in setting the ARS seed
 maxsize = sys.maxsize
+
 
 
 # def draw_gamma_ras(a, theta, size=1):
@@ -68,51 +70,36 @@ def draw_Beta_dist(delta_a, delta_b):
     return beta_dist.rvs(delta_a, delta_b)
 
 
-# @jit(nogil=True,)
-# def Asymmetric_Gassian_Distribution_pdf(x_k, mu_jk, s_ljk, s_rjk, mu_jk_irr, s_ljk_irr, s_rjk_irr, rho):
-#     '''
-#     Asymmetric Gassuian distribution pdf for all observations
-#     '''
-#     y_k = np.zeros(x_k.shape[0])
-#     for i, xik in enumerate(x_k):
-#         if xik < mu_jk:
-#             y_k[i] += rho * np.sqrt(2/np.pi)/(np.power(s_ljk, -0.5) + np.power(s_rjk, -0.5))\
-#                    * np.exp(- 0.5 * s_ljk * (xik- mu_jk)**2)
-#         else:
-#             y_k[i] += rho * np.sqrt(2/np.pi)/(np.power(s_ljk, -0.5) + np.power(s_rjk, -0.5))\
-#                    * np.exp(- 0.5 * s_rjk * (xik- mu_jk)**2)
-#         if xik < mu_jk_irr:
-#             y_k[i] += (1 - rho) * np.sqrt(2/np.pi)/(np.power(s_ljk_irr, -0.5) + np.power(s_rjk_irr, -0.5))\
-#                    * np.exp(- 0.5 * s_ljk_irr * (xik- mu_jk_irr)**2)
-#         else:
-#             y_k[i] += (1 - rho) * np.sqrt(2/np.pi)/(np.power(s_ljk_irr, -0.5) + np.power(s_rjk_irr, -0.5))\
-#                    * np.exp(- 0.5 * s_rjk_irr * (xik- mu_jk_irr)**2)
-#     return y_k
-
-
+@jit(nogil=True,)
 def AGD_pdf(x_k, mu, s_l, s_r):
     '''
     Asymmetric Gassuian distribution pdf for single observation
     '''
+    y = 0.0
     if x_k < mu:
-        return np.sqrt(2 / np.pi) / (np.power(s_l, -0.5) + np.power(s_r, -0.5)) \
+        y =  np.sqrt(2 / np.pi) / (np.power(s_l, -0.5) + np.power(s_r, -0.5)) \
                * np.exp(- 0.5 * s_l * (x_k - mu) ** 2)
     else:
-        return np.sqrt(2 / np.pi) / (np.power(s_l, -0.5) + np.power(s_r, -0.5)) \
+        y = np.sqrt(2 / np.pi) / (np.power(s_l, -0.5) + np.power(s_r, -0.5)) \
                * np.exp(- 0.5 * s_r * (x_k - mu) ** 2)
+    if y < 0.000001:
+        return 0.000001
+    else:
+        return y
 
-
-def AGD_pdf_feature_selction(x, j, D, rho, mu, s_l, s_r, mu_irr, s_l_irr, s_r_irr):
+def AGD_pdf_feature_selction(x, j, D, rho, mu, s_l, s_r, mu_irr, s_irr):
     '''
     Asymmetric Gassuian distribution pdf with feature selection
     '''
-    y = mpmath.mpf(1.0)
+    # y = mpmath.mpf(1.0)
+    y = 1.0
     for k in range(D):
         y *= (rho[j, k] * AGD_pdf(x[k], mu[j, k], s_l[j, k], s_r[j, k]) + (1-rho[j, k])*
-              AGD_pdf(x[k], mu_irr[j, k], s_l_irr[j, k], s_r_irr[j, k]))
-    return y
-
-
+              norm.pdf(x[k], mu_irr[j, k], s_irr[j, k]))
+    if y < 0.000001:
+        return 0.000001
+    else:
+        return y
 
 
 def compare_mu_jk(mu_jk, previous_mu_jk, s_ljk, s_rjk, mu_jk_irr, s_ljk_irr, s_rjk_irr, r, lam, z, j, k, X):
@@ -125,7 +112,7 @@ def compare_mu_jk(mu_jk, previous_mu_jk, s_ljk, s_rjk, mu_jk_irr, s_ljk_irr, s_r
         compared_log_likelihood += z[i, j, k] * (np.log(AGD_pdf(x_i[k], mu_jk, s_ljk, s_rjk)) -
                                                  np.log(AGD_pdf(x_i[k], previous_mu_jk, s_ljk, s_rjk))) + \
                                    (1-z[i, j, k]) * (np.log(AGD_pdf(x_i[k], mu_jk_irr, s_ljk_irr, s_rjk_irr)) -
-                                                 np.log(AGD_pdf(x_i[k], mu_jk_irr, s_ljk_irr, s_rjk_irr)))
+                                                     np.log(AGD_pdf(x_i[k], mu_jk_irr, s_ljk_irr, s_rjk_irr)))
     likelihood_ratio = np.exp(compared_log_likelihood)
     prior = draw_normal(loc=lam[k], scale=1/r[k])[0] / draw_normal(loc=lam[k], scale=1/r[k])[0]
     return likelihood_ratio * prior
@@ -135,7 +122,7 @@ def MH_Sampling_posterior_mu_jk(mu_jk, s_ljk, s_rjk, mu_jk_irr, s_ljk_irr, s_rjk
     '''
     Metropolis Hastings sampling for the postiors of mu_jk parameter
     '''
-    n = 750
+    n = 600
     x = mu_jk
     vec = []
     vec.append(x)
@@ -160,7 +147,7 @@ def compare_s_ljk(s_ljk, previous_s_ljk, mu_jk, s_rjk, mu_jk_irr, s_ljk_irr, s_r
         compared_log_likelihood += z[i, j, k]*(np.log(AGD_pdf(x_i[k], mu_jk, s_ljk, s_rjk)) -
                                                np.log(AGD_pdf(x_i[k], mu_jk, previous_s_ljk, s_rjk)))+ \
                                    (1-z[i, j, k])*(np.log(AGD_pdf(x_i[k], mu_jk_irr, s_ljk_irr, s_rjk_irr))-
-                                                 np.log(AGD_pdf(x_i[k], mu_jk_irr, s_ljk_irr, s_rjk_irr)))
+                                                   np.log(AGD_pdf(x_i[k], mu_jk_irr, s_ljk_irr, s_rjk_irr)))
     likelihood_ratio = np.exp(compared_log_likelihood)
     prior = draw_gamma(beta[k]/2, 2/(beta[k]*w[k]))[0] / draw_gamma(beta[k]/2, 2/(beta[k]*w[k]))[0]
     return likelihood_ratio * prior
@@ -170,7 +157,7 @@ def MH_Sampling_posterior_sljk(mu_jk, s_ljk, s_rjk, mu_jk_irr, s_ljk_irr, s_rjk_
     '''
     Metropolis Hastings sampling for the postiors of s_ljk parameter
     '''
-    n = 1000
+    n = 2000
     x = s_ljk
     vec = []
     vec.append(x)
@@ -197,7 +184,7 @@ def compare_s_rjk(s_rjk, previous_s_rjk, mu_jk, s_ljk, mu_jk_irr, s_ljk_irr, s_r
         compared_log_likelihood += z[i, j, k] * (np.log(AGD_pdf(x_i[k], mu_jk, s_ljk, s_rjk))-
                                                  np.log(AGD_pdf(x_i[k], mu_jk, s_ljk, previous_s_rjk))) + \
                                    (1-z[i, j, k]) * (np.log(AGD_pdf(x_i[k], mu_jk_irr, s_ljk_irr, s_rjk_irr)) -
-                                                 np.log(AGD_pdf(x_i[k], mu_jk_irr, s_ljk_irr, s_rjk_irr)))
+                                                     np.log(AGD_pdf(x_i[k], mu_jk_irr, s_ljk_irr, s_rjk_irr)))
     likelihood_ratio = np.exp(compared_log_likelihood)
     prior = draw_gamma(beta[k]/2, 2/(beta[k]*w[k]))[0] / draw_gamma(beta[k]/2, 2/(beta[k]*w[k]))[0]
     return likelihood_ratio * prior
@@ -207,7 +194,7 @@ def MH_Sampling_posterior_srjk(mu_jk, s_ljk, s_rjk, mu_jk_irr, s_ljk_irr, s_rjk_
     '''
     Metropolis Hastings sampling for the postiors of s_rjk parameter
     '''
-    n = 2500
+    n = 2000
     x = s_rjk
     vec = []
     vec.append(x)
@@ -222,7 +209,6 @@ def MH_Sampling_posterior_srjk(mu_jk, s_ljk, s_rjk, mu_jk_irr, s_ljk_irr, s_rjk_
             x = candidate
             vec.append(x)
     return vec[-1]
-
 
 
 def compare_delta_a(delta_a, previous_delta_a, delta_b, rho, k, M):
@@ -258,6 +244,7 @@ def MH_Sampling_posterior_delta_a(delta_a, delta_b, rho, k, M):
             x = candidate
             vec.append(x)
     return vec[-1]
+
 
 def compare_delta_b(delta_b, previous_delta_b, delta_a, rho, k, M):
     '''
@@ -296,6 +283,7 @@ def MH_Sampling_posterior_delta_b(delta_a, delta_b, rho, k, M):
 
 @jit(nogil=True,)
 def Asymmetric_Gassian_Distribution_pdf(x_k, mu_jk, s_ljk, s_rjk):
+
     y_k = np.zeros(x_k.shape[0])
     for i, xik in enumerate(x_k):
         if xik < mu_jk:
@@ -307,7 +295,7 @@ def Asymmetric_Gassian_Distribution_pdf(x_k, mu_jk, s_ljk, s_rjk):
     return y_k
 
 
-def integral_approx(X, lam, r, beta_l, beta_r, w_l, w_r, size=15):
+def integral_approx(X, lam, r, beta_l, beta_r, w_l, w_r, G=1, size=15):
     """
     estimates the integral, eq 17 (Rasmussen 2000)
     """
@@ -322,14 +310,34 @@ def integral_approx(X, lam, r, beta_l, beta_r, w_l, w_r, size=15):
         s_r = np.array([np.squeeze(draw_gamma(beta_r[k] / 2, 2 / (beta_r[k] * w_r[k]))) for k in range(D)])
         ini = np.ones(len(X))
         for k in range(D):
-            # use metropolis-hastings algorithm to draw sampling from AGD
-            # the size parameter is the required sampling number which is equal to the dataset's number
-            # the n parameter is MH algorithm itering times,because the acceptance rate should be 25%-40%
             temp_para = Asymmetric_Gassian_Distribution_pdf(X[:, k], mu[k], s_l[k], s_r[k])
             ini *= temp_para
         temp += ini
         i += 1
     return temp/float(size)
+
+
+# @jit(nogil=True,)
+# def Asymmetric_Gassian_Distribution_pdf(x_k, mu_jk, s_ljk, s_rjk, mu_jk_irr, s_ljk_irr, s_rjk_irr, rho):
+#     '''
+#     Asymmetric Gassuian distribution pdf for all observations
+#     '''
+#     y_k = np.zeros(x_k.shape[0])
+#     for i, xik in enumerate(x_k):
+#         if xik < mu_jk:
+#             y_k[i] += rho * np.sqrt(2/np.pi)/(np.power(s_ljk, -0.5) + np.power(s_rjk, -0.5))\
+#                    * np.exp(- 0.5 * s_ljk * (xik- mu_jk)**2)
+#         else:
+#             y_k[i] += rho * np.sqrt(2/np.pi)/(np.power(s_ljk, -0.5) + np.power(s_rjk, -0.5))\
+#                    * np.exp(- 0.5 * s_rjk * (xik- mu_jk)**2)
+#         if xik < mu_jk_irr:
+#             y_k[i] += (1 - rho) * np.sqrt(2/np.pi)/(np.power(s_ljk_irr, -0.5) + np.power(s_rjk_irr, -0.5))\
+#                    * np.exp(- 0.5 * s_ljk_irr * (xik- mu_jk_irr)**2)
+#         else:
+#             y_k[i] += (1 - rho) * np.sqrt(2/np.pi)/(np.power(s_ljk_irr, -0.5) + np.power(s_rjk_irr, -0.5))\
+#                    * np.exp(- 0.5 * s_rjk_irr * (xik- mu_jk_irr)**2)
+#     return y_k
+
 
 # def integral_approx(X, lam, r, beta_l, beta_r, w_l, w_r, delta_a, delta_b, size=20):
 #     """
@@ -357,24 +365,27 @@ def integral_approx(X, lam, r, beta_l, beta_r, w_l, w_r, size=15):
 #             ini *= temp_para
 #         temp += ini
 #         i += 1
-#     return temp/float(size)
+#     return temp / float(size)
 
 
 def log_p_alpha(alpha, k, N):
     """
-    the log of eq15 (Rasmussen 2000)
+    the log of alpha posteriors
     """
     return (k - 1.5)*np.log(alpha) - 0.5/alpha + special.gammaln(alpha) - special.gammaln(N + alpha)
 
 
 def log_p_alpha_prime(alpha, k, N):
     """
-    the derivative (wrt alpha) of the log of eq 15 (Rasmussen 2000)
+    the derivative log alpha posteriors
     """
     return (k - 1.5)/alpha + 0.5/(alpha*alpha) + special.psi(alpha) - special.psi(alpha + N)
 
 
 def log_p_beta(beta, M, cumculative_sum_equation=1):
+    """
+    the log of beta posteriors
+    """
     return -2*M*special.gammaln(beta/2) \
         - 0.5/beta \
         + (beta*M-1.5)*np.log(beta/2) \
@@ -382,6 +393,9 @@ def log_p_beta(beta, M, cumculative_sum_equation=1):
 
 
 def log_p_beta_prime(beta, M, cumculative_sum_equation=1):
+    """
+    the derivative log beta posteriors
+    """
     return -2*M*special.psi(0.5*beta) \
         + 0.5/beta**2 \
         + M*np.log(0.5*beta) \
@@ -391,14 +405,16 @@ def log_p_beta_prime(beta, M, cumculative_sum_equation=1):
 
 def draw_alpha(k, N, size=1):
     """
-    draw alpha from posterior (depends on k, N), eq 15 (Rasmussen 2000), using ARS
-    Make it robust with an expanding range in case of failure
+    draw alpha from posteriors
     """
     ars = ARS(log_p_alpha, log_p_alpha_prime, xi=[0.1, 5], lb=0, ub=np.inf, k=k, N=N)
     return ars.draw(size)
 
 
 def draw_beta_ars(w, s, s_irr, M, k, size=1):
+    """
+    draw beta from posteriors
+    """
     D = 2
     cumculative_sum_equation = 0
     for sj in s:
@@ -409,10 +425,9 @@ def draw_beta_ars(w, s, s_irr, M, k, size=1):
         cumculative_sum_equation += np.log(sj_irr[k])
         cumculative_sum_equation -= w[k]*sj_irr[k]
     lb = D
-    ars = ARS(log_p_beta, log_p_beta_prime, xi=[lb + 1], lb=lb, ub=float("inf"), \
+    ars = ARS(log_p_beta, log_p_beta_prime, xi=[lb + 15], lb=lb, ub=float("inf"), \
              M=M, cumculative_sum_equation=cumculative_sum_equation)
     return ars.draw(size)
-
 
 
 def draw_indicator(pvec):
@@ -433,23 +448,21 @@ def draw_indicator(pvec):
     return res
 
 
-def draw_posterior_z(X, pi, rho, mu, s_l, s_r, mu_irr, s_l_irr, s_r_irr, N, M, D):
+def draw_posterior_z(X, pi, rho, mu, s_l, s_r, mu_irr, s_irr, N, M, D):
     Z_ij = np.zeros((N, M), dtype=mpmath.mpf)
     Z_i = np.zeros(N, dtype=mpmath.mpf)
     Z_ij_posteriors = np.zeros((N, M), dtype=mpmath.mpf)
     posterior_z = np.zeros((N, M, D))
     for i in range(N):
         for j in range(M):
-            Z_ij[i, j] = pi[j] * AGD_pdf_feature_selction(X[i], j, D, rho, mu, s_l, s_r, mu_irr, s_l_irr,
-                                                          s_r_irr) + 0.000001
+            Z_ij[i, j] = pi[j] * AGD_pdf_feature_selction(X[i], j, D, rho, mu, s_l, s_r, mu_irr, s_irr)
         Z_i[i] = np.sum(Z_ij[i])
         Z_ij_posteriors[i] = Z_ij[i] / Z_i[i]
     for i in range(N):
         for j in range(M):
             for k in range(D):
                 rele_result = rho[j, k] * AGD_pdf(X[i, k], mu[j, k], s_l[j, k], s_r[j, k]) + 0.000001
-                irr_result = (1 - rho[j, k]) * AGD_pdf(X[i, k], mu_irr[j, k], s_l_irr[j, k], s_r_irr[j, k]) + 0.000001
+                irr_result = (1 - rho[j, k]) * norm.pdf(X[1, k], mu_irr[j, k], s_irr[j, k]) + 0.000001
                 # posterior_z[i, j, k] = rho[j, k] * AGD_pdf(X[i, k], mu[j, k], s_l[j, k], s_r[j, k]) * Z_ij_posteriors[i, j]
                 posterior_z[i, j, k] = (Z_ij_posteriors[i, j] * rele_result) /(rele_result + irr_result)
     return posterior_z
-